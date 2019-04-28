681bc281e933c221c452ce070770a34539d392e1
Garrett Irwin <garrettirwin@hotmail.com>
2019-04-28 16:07:09 +1000

    Part 2 complete.

--- main.go
+++ main.go
@@ -3,47 +3,68 @@ package main
 import (
 	"bufio"
 	"encoding/csv"
+	"flag"
 	"fmt"
-	"io"
 	"log"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
-	var answers []string
-	var correctAnswers []string
 	correct := 0
+	timeLimit := flag.Int("limit", 30, "the time limit for this quiz in seconds")
+
 	csvFile, err := os.Open("problems.csv")
 	if err != nil {
 		log.Fatalln("Couldn't open the csv file", err)
 	}
 	reader := csv.NewReader(csvFile)
+	lines, err := reader.ReadAll()
+	problems := parseLines(lines)
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
 
-	for {
-		row, err := reader.Read()
-		if err != nil {
-			if err == io.EOF {
-				break
+	//a label for a loop to easily break out of it
+problemLoop:
+	for i, p := range problems {
+		fmt.Printf("Question %d: %s = ", i+1, p.q)
+		//creates a channel to listen for an answer
+		answerCh := make(chan string)
+		go func() {
+			reader := bufio.NewReader(os.Stdin)
+			ans, err := reader.ReadString('\n')
+			ans = strings.TrimSuffix(ans, "\n")
+			if err != nil {
+				log.Println(err)
 			}
-			log.Fatal(err)
+			//send the answer to the listening channel
+			answerCh <- ans
+		}()
+		select {
+		case <-timer.C:
+			fmt.Printf("\nYou Got %d of %d answers correct", correct, len(problems))
+			break problemLoop
+		case ans := <-answerCh:
+			if ans == p.a {
+				correct++
+			}
 		}
-		fmt.Printf("%s = ", row[0])
-		correctAnswers = append(correctAnswers, row[1])
 
-		reader := bufio.NewReader(os.Stdin)
-		ans, err := reader.ReadString('\n')
-		ans = strings.TrimSuffix(ans, "\n")
-		if err != nil {
-			log.Println(err)
-		}
-		answers = append(answers, ans)
 	}
+}
 
-	for i := 0; i < len(correctAnswers); i++ {
-		if correctAnswers[i] == answers[i] {
-			correct++
+func parseLines(lines [][]string) []problems {
+	ret := make([]problems, len(lines))
+	for i := 0; i < len(lines); i++ {
+		ret[i] = problems{
+			q: lines[i][0],
+			a: strings.TrimSpace(lines[i][1]),
 		}
 	}
-	fmt.Printf("You Got %d of %d answers correct", correct, len(correctAnswers))
+	return ret
+}
+
+type problems struct {
+	q string
+	a string
 }
